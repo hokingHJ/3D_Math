246dd629636e5f7e426e78f26ae017392805a059
hoking <1808591593@qq.com>
2018-02-05 11:30:04 +0800

    add some functions

--- 3DMathBasic/Vector_3D.h
+++ 3DMathBasic/Vector_3D.h
@@ -3,37 +3,71 @@
 
 #include <math.h>
 /*
-		This class is crated for a 3D vector,a mathematical concept,that contain three numbers
-	(x,y,z) respectively representing different dimensions.
-		I think the variables in the class should be modified by private,that symblizes a good 
-	encapsulation of the class.
+	This class is crated for a 3D vector,a mathematical concept,that contain three numbers
+(x,y,z) respectively representing different dimensions.
+	I think the variables in the class should be modified by private,that symblizes a good 
+encapsulation of the class.
 */
-
 class Vector_3D {
 private:
-	float x, y, z;
+	double x, y, z;
 public:
 	Vector_3D(){}
+	//Copy-constructor.
 	Vector_3D(const Vector_3D &vector) : x(vector.x),y(vector.y),z(vector.z){}
-	Vector_3D(float a, float b, float c) : x(a), y(b), z(c){}
+	Vector_3D(double x, double y, double z) : x(x), y(y), z(z){}
 	void zero() {
-		x = y = z = 0.0f;
+		x = y = z = 0.0;
 	}
 	Vector_3D operator -() {
 		return Vector_3D(-x,-y,-z);
 	}
-	float getX() const {
-		return x;
+	Vector_3D operator *(double coef) const{
+		return Vector_3D(coef * x, coef * y, coef * z);
 	}
-	float getY()  const{
-		return y;
+	Vector_3D operator /(double coef) const{
+		float reciprocal = 1.0f / coef;
+		return Vector_3D(reciprocal * x,reciprocal * y,reciprocal * z);
 	}
-	float getZ() const {
-		return z;
+	//It just returns a new Vector_3D object and do not modify anything of this object.
+	Vector_3D operator -(const Vector_3D &vector) {
+		return Vector_3D(x - vector.getX(),y - vector.getY(),z - vector.getZ());
+	}
+	Vector_3D operator +(const Vector_3D &vector) {
+		return Vector_3D(x + vector.getX(), y + vector.getY(), z + vector.getZ());
+	}
+	//The mehtod will chang the values of this object.So no 'cosnt'.
+	Vector_3D operator *=(float coef) {
+		//The operator * has been overload and I don't want to chang the value respectively.like x*=coef.
+		*this = *this * coef;
+		return *this;
+	}
+	//The mehtod will chang the values of this object.So no 'cosnt'.
+	Vector_3D operator /=(float coef) {
+		//The same reason above.
+		*this = *this / coef;
+		return *this;
+	}
+	void normalize() {
+		*this /= vectorMag();
+	}
+	double vectorMag() const{
+		return sqrt(x*x+y*y+z*z);
+	}
+	//This method should be describe by "const",althouth,it just return a value rather than a reference.
+	//It is not necessary to worry about modification.
+	double getX() const {
+		return this->x;
+	}
+	double getY()  const{
+		return this->y;
+	}
+	double getZ() const {
+		return this->z;
 	}
 };
-inline float vectorMag(Vector_3D v) {
-	return sqrt(v.getX()* v.getX() + v.getY() * v.getY() + v.getZ() * v.getZ());
+inline Vector_3D operator *(double coef,const Vector_3D &vector) {
+	return Vector_3D(coef * vector.getX(), coef * vector.getY(), coef * vector.getZ());
 }
 #endif
 
--- 3DMathBasic/main.cpp
+++ 3DMathBasic/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Vector_3D.h"
 
 using namespace std;
@@ -11,23 +12,42 @@ using namespace std;
 	as you can see.Fortunately,it really works good.I am glad to have a grate start.
 
 */
-void print_v(Vector_3D &vector) {
-	cout << "[" << vector.getX() << "," << vector.getY()<< "," << vector.getZ()<< "]" << endl;
+void print_v(string name,Vector_3D &vector) {
+	cout << name<<":"<<"[" << vector.getX() << "," << vector.getY()<< "," << vector.getZ()<< "]" << endl;
 }
-
 int main() {
 	cout << "Everything is OK!!!" << endl;
 	Vector_3D v1(21,34,12);
-	print_v(v1);
+	print_v("v1",v1);
 	Vector_3D v2(v1);
-	print_v(v2);
+	print_v("v2",v2);
 	v2.zero();
-	print_v(v2);
-	float magic = vectorMag(v1);
-	cout << magic << endl;
+	print_v("After zero() v2",v2);
+	double magic = v1.vectorMag();
+	cout << "v1 magic :"<<magic << endl;
 	Vector_3D v3 = -v1;
-	print_v(v3);
-	print_v(v1);
+	print_v("v3(-v1)",v3);
+	Vector_3D v4(2, 4, 6);
+	Vector_3D v5 = 2 * v4;
+	print_v("2*v4(2,4,6)",v5);
+	v5 = v4 * 2;
+	print_v("v4(2,4,6)*2",v5);
+	Vector_3D v6 = v4 / 2;
+	print_v("v4/2",v6);
+
+	v5 *= 2;
+	v6 /= 2;
+	print_v("v5*=2",v5);
+	print_v("v6/=2",v6);
+	Vector_3D v7(1.8,2.2,3.7);
+	Vector_3D v8(2.2,5.8,9.3);
+	Vector_3D v9 = v7 - v8;
+	Vector_3D v10 = v7 + v8;
+	print_v("v7-v8",v9);
+	print_v("v7+v8",v10);
+	Vector_3D v11(3,4,5);
+	v11.normalize();
+	print_v("v11(3,4,5) normalize()",v11);
 	system("pause");
 	return 0;
 };
